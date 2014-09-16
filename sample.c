01a2dafeaadfbf1a26d143bedcf7904c5dc3382d
Kamijo Takuji <takuji.kamijou@tel.com>
2014-09-16 15:44:10 +0800

    Sample Code Version 1.00

---
+++ sample.c
@@ -0,0 +1,43 @@
+/******************/
+/*  バブルソート  */
+/******************/
+#include <stdio.h>
+#define  MAX  10
+void  main( )
+{
+  int  data[MAX]={ 80,5,36,23,12,100,45,9,1,78 };
+  int  n,i,w;
+
+    printf("\n☆バブルソート☆\n");
+
+    printf("\nソート前\n");
+    for( i=0; i<MAX; i++ )
+      { printf("%d ",data[i]); }
+
+    printf("\n\n途中経過\n");
+
+    for( n=MAX; n>1; n-- )              // 未整列データ数
+      {
+        for( i=0; i<n-1; i++ )
+          {
+            if ( data[i]>data[i+1] )    // 次のデータが小さい
+              {
+                //--入れ替え--
+                w=data[i];
+                data[i]=data[i+1];
+                data[i+1]=w;
+              }
+          }
+
+        for( i=0; i<MAX; i++ )
+          { printf("%d ",data[i]); }
+        printf("\n");
+      }
+
+    printf("\nソート後\n");
+    for( i=0; i<MAX; i++ )
+      { printf("%d ",data[i]); }
+
+    printf("\n");
+    fflush(stdout);
+}
